4a554957e06aaf63918b3f3c919dbace8a1c0b57
castroreyrobert <reyrobertcastro@gmail.com>
2016-10-16 21:23:26 +0800

    4. Designed the fragment_note_edit.xml

---
+++ app/src/main/java/com/example/castroreyrobert/fragmentlistview/NoteEditFragment.java
@@ -0,0 +1,27 @@
+package com.example.castroreyrobert.fragmentlistview;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class NoteEditFragment extends Fragment {
+
+
+    public NoteEditFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_note_edit, container, false);
+    }
+
+}
